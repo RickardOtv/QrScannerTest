5314b4eee376ce8d2c41938bf43a87ef95cd2709
rickardotv <rickard.0413@gmail.com>
2023-06-17 11:22:31 +0200

    Initial Commit

---
+++ CodeScanningTest/CodeScanningTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeScanningTestApp.swift
+//  CodeScanningTest
+//
+//  Created by  Rickard Otvos on 2023-06-17.
+//
+
+import SwiftUI
+
+@main
+struct CodeScanningTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
